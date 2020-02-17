f273a1a02ef688522f64685df00d0e5047a5d680
Your Name <rkbjr11@gmail.com>
2020-02-17 14:17:05 +0600

    Updates

---
+++ precache-manifest.74580e6623997f10eb49de2f85f3d468.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff46da05e4f20ac8db8da237dceaa365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b83aa409939af4805567",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "3d8030af7fd66678f26d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b83aa409939af4805567",
+    "url": "/static/js/2.740082e1.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.740082e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8030af7fd66678f26d",
+    "url": "/static/js/main.44ed6e89.chunk.js"
+  },
+  {
+    "revision": "f1b990954651ad3646f5",
+    "url": "/static/js/runtime-main.cd111287.js"
+  },
+  {
+    "revision": "f744cebb1e5fe1be8301e6cc7ed76d48",
+    "url": "/static/media/add_book.f744cebb.PNG"
+  },
+  {
+    "revision": "700239e0ce27c67ab6ac41ed6b23306b",
+    "url": "/static/media/add_fund.700239e0.PNG"
+  },
+  {
+    "revision": "284e66d4b31c175085c33f6e1639431e",
+    "url": "/static/media/addstuden.284e66d4.PNG"
+  },
+  {
+    "revision": "4fc5c2028ca4b1df483b03227deccb9a",
+    "url": "/static/media/admin_home.4fc5c202.PNG"
+  },
+  {
+    "revision": "5412a48a1d19a2a0f0c63a54be9a2fa1",
+    "url": "/static/media/agent login.5412a48a.PNG"
+  },
+  {
+    "revision": "e8601f15ed7d9c8e9e47bbb4c25327e0",
+    "url": "/static/media/blood_donate.e8601f15.PNG"
+  },
+  {
+    "revision": "4a396504d79219a75b66c0bf5db5453e",
+    "url": "/static/media/blood_req.4a396504.PNG"
+  },
+  {
+    "revision": "6283a6faccf25efe99a93e05f30f763d",
+    "url": "/static/media/blood_serach.6283a6fa.PNG"
+  },
+  {
+    "revision": "b8a616c2e4034573b09928ffe7cad2bf",
+    "url": "/static/media/blood_serach1.b8a616c2.PNG"
+  },
+  {
+    "revision": "e09f4fa12b78adcaa96c49c1768b4dfc",
+    "url": "/static/media/book_list.e09f4fa1.PNG"
+  },
+  {
+    "revision": "3b2c5f2896fb65df1a439105dcb746bb",
+    "url": "/static/media/cart.3b2c5f28.PNG"
+  },
+  {
+    "revision": "9672d0a1c1f47ba563dc9198241de350",
+    "url": "/static/media/cart_confirm.9672d0a1.PNG"
+  },
+  {
+    "revision": "0751a8bfc62431db777ff1f0c99a2bce",
+    "url": "/static/media/deposit_his.0751a8bf.PNG"
+  },
+  {
+    "revision": "e44812d6804800fde03dc57192941acd",
+    "url": "/static/media/discount.e44812d6.PNG"
+  },
+  {
+    "revision": "b558595fee4095a395d01749fbd74813",
+    "url": "/static/media/dofdr.b558595f.PNG"
+  },
+  {
+    "revision": "9741b0c5d9c5f1592abe0164da00775c",
+    "url": "/static/media/donate_HISTORY.9741b0c5.PNG"
+  },
+  {
+    "revision": "3c0cbdcda921f3bf27c23c57c6abed4c",
+    "url": "/static/media/donor_home.3c0cbdcd.PNG"
+  },
+  {
+    "revision": "1dadbb7159c6c4f1d7c9ad06a91749de",
+    "url": "/static/media/dps.1dadbb71.PNG"
+  },
+  {
+    "revision": "8d409ff3fb899422455eae8be2edc193",
+    "url": "/static/media/expense_his.8d409ff3.PNG"
+  },
+  {
+    "revision": "e333c310fa92379cd9b885889b1a0dcf",
+    "url": "/static/media/find.e333c310.PNG"
+  },
+  {
+    "revision": "14198f663e003f1babab8cfdc556e7bc",
+    "url": "/static/media/fund_transfer.14198f66.PNG"
+  },
+  {
+    "revision": "96a3be909179b126085ea50d2703dc83",
+    "url": "/static/media/homa.96a3be90.PNG"
+  },
+  {
+    "revision": "c80c5551a166ebf2ddc0ab504a7362f6",
+    "url": "/static/media/home.c80c5551.PNG"
+  },
+  {
+    "revision": "aad3e862b9a43938d15c044408de9f1b",
+    "url": "/static/media/home_showroom.aad3e862.PNG"
+  },
+  {
+    "revision": "a465329fc41ed37be9694b2d099b0652",
+    "url": "/static/media/image.a465329f.PNG"
+  },
+  {
+    "revision": "df8ea2bc0a0f17b92de38137459a8b50",
+    "url": "/static/media/info.df8ea2bc.PNG"
+  },
+  {
+    "revision": "b04234b617987f396822838691d7fc5c",
+    "url": "/static/media/invoice.b04234b6.PNG"
+  },
+  {
+    "revision": "a63c4d9fbc7e6e79a6d769e1e30aa11e",
+    "url": "/static/media/issue.a63c4d9f.PNG"
+  },
+  {
+    "revision": "2eb854de102493d3eb230614ab8786a0",
+    "url": "/static/media/issue_book list.2eb854de.PNG"
+  },
+  {
+    "revision": "1f2d1004cf6b5d133a68b7a3de866436",
+    "url": "/static/media/make_payment.1f2d1004.PNG"
+  },
+  {
+    "revision": "fdba2590e6e6a4f435e1fe2a2b2185e6",
+    "url": "/static/media/member_list.fdba2590.PNG"
+  },
+  {
+    "revision": "276296d43eacb9dc7d41aab27b8d331f",
+    "url": "/static/media/order_successfull.276296d4.PNG"
+  },
+  {
+    "revision": "07ae4b54770291d2d516aaef852ce6e2",
+    "url": "/static/media/print_copy.07ae4b54.PNG"
+  },
+  {
+    "revision": "f817dcde6f6d0877ce0ea85979e8f571",
+    "url": "/static/media/product_stock.f817dcde.PNG"
+  },
+  {
+    "revision": "ae381c23f9940a9eb8fc219f099b2253",
+    "url": "/static/media/req_details.ae381c23.PNG"
+  },
+  {
+    "revision": "d0222dba624fc0d88254323be4e65ec5",
+    "url": "/static/media/return.d0222dba.PNG"
+  },
+  {
+    "revision": "bad0f795a09e0d59ba8df06adf5d686b",
+    "url": "/static/media/sales_report.bad0f795.PNG"
+  },
+  {
+    "revision": "5f18daafab3729ef1300173e1c594bd4",
+    "url": "/static/media/send_blood_req.5f18daaf.PNG"
+  },
+  {
+    "revision": "f9b322c14d92745bcff7490ee78c931e",
+    "url": "/static/media/std_list.f9b322c1.PNG"
+  },
+  {
+    "revision": "8bfe9eaf4c56c63b5f2ad44b3d6b9f45",
+    "url": "/static/media/summary.8bfe9eaf.PNG"
+  },
+  {
+    "revision": "9fa697102442383096d3b7508becf712",
+    "url": "/static/media/withdrow.9fa69710.PNG"
+  },
+  {
+    "revision": "24821e4c05f972ac55b1d50c3296077a",
+    "url": "/static/media/withdrow_historyy.24821e4c.PNG"
+  }
+]);
