9060a51b812933d42cf7cb18185d95dc9e2fccd9
hossainrkb <39438732+hossainrkb@users.noreply.github.com>
2020-02-18 13:50:28 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,102 +1,68 @@
-import React, { Component } from "react";
+import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import NavTop from "./components/navtop";
-import Education from "./components/eductaions";
-import Framework from "./components/framework";
-import Introduction from "./components/Introduction";
-import Programming_languague from "./components/programming_languague";
-import Project from "./components/projects";
+import NavTop from './components/navtop'
+import Education from './components/eductaions'
+import Framework from './components/framework'
+import Introduction from './components/Introduction'
+import Programming_languague from './components/programming_languague'
+import Project from './components/projects'
 import Project_details from "./components/pro_details";
 import Experience from "./components/experience";
 import { Redirect } from "react-router-dom";
-
-
-
-
-
-class App extends Component {
-  state = {
-    check:0
+function App() {
+  if (window.location.href !== "http://localhost:3000/rakib_portfolio/") {
+return (
+  <BrowserRouter>
+    <Redirect to="/rakib_portfolio/" />
+    <div className="container">
+      <NavTop />
+      <Switch>
+        <Route path="/rakib_portfolio/" exact component={Introduction} />
+        <Route path="/rakib_portfolio/edu/" exact component={Education} />
+        <Route path="/rakib_portfolio/frmwrk/" exact component={Framework} />
+        <Route
+          path="/rakib_portfolio/pro_lan/"
+          exact
+          component={Programming_languague}
+        />
+        <Route path="/rakib_portfolio/proj/" exact component={Project} />
+        <Route path="/rakib_portfolio/exp/" exact component={Experience} />
+        <Route
+          path="/rakib_portfolio/pro/details/:pd_id/"
+          exact
+          component={Project_details}
+        />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+    
   }
-  componentWillMount(){
-     if (
-       window.location.href !== "https://hossainrkb.github.io/rakib_portfolio/"
-     ) {
-       this.setState({
-         check: 1
-       });
-     }
-  }
-  render() {
-     if (this.state.check ==1) {
-       return (
-         <BrowserRouter>
-           <Redirect to="/rakib_portfolio/" />
-           <div className="container">
-             <NavTop />
-             <Switch>
-               <Route path="/rakib_portfolio/" exact component={Introduction} />
-               <Route
-                 path="/rakib_portfolio/edu/"
-                 exact
-                 component={Education}
-               />
-               <Route
-                 path="/rakib_portfolio/frmwrk/"
-                 exact
-                 component={Framework}
-               />
-               <Route
-                 path="/rakib_portfolio/pro_lan/"
-                 exact
-                 component={Programming_languague}
-               />
-               <Route path="/rakib_portfolio/proj/" exact component={Project} />
-               <Route
-                 path="/rakib_portfolio/exp/"
-                 exact
-                 component={Experience}
-               />
-               <Route
-                 path="/rakib_portfolio/pro/details/:pd_id/"
-                 exact
-                 component={Project_details}
-               />
-             </Switch>
-           </div>
-         </BrowserRouter>
-       );
-     }
-     return (
-       <BrowserRouter>
-         <div className="container">
-           <NavTop />
-           <Switch>
-             <Route path="/rakib_portfolio/" exact component={Introduction} />
-             <Route path="/rakib_portfolio/edu/" exact component={Education} />
-             <Route
-               path="/rakib_portfolio/frmwrk/"
-               exact
-               component={Framework}
-             />
-             <Route
-               path="/rakib_portfolio/pro_lan/"
-               exact
-               component={Programming_languague}
-             />
-             <Route path="/rakib_portfolio/proj/" exact component={Project} />
-             <Route path="/rakib_portfolio/exp/" exact component={Experience} />
-             <Route
-               path="/rakib_portfolio/pro/details/:pd_id/"
-               exact
-               component={Project_details}
-             />
-           </Switch>
-         </div>
-       </BrowserRouter>
-     );
-   
-  }
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <NavTop />
+     
+        <Switch>
+          <Route path="/rakib_portfolio/" exact component={Introduction} />
+          <Route path="/rakib_portfolio/edu/" exact component={Education} />
+          <Route path="/rakib_portfolio/frmwrk/" exact component={Framework} />
+          <Route
+            path="/rakib_portfolio/pro_lan/"
+            exact
+            component={Programming_languague}
+          />
+          <Route path="/rakib_portfolio/proj/" exact component={Project} />
+          <Route path="/rakib_portfolio/exp/" exact component={Experience} />
+          <Route
+            path="/rakib_portfolio/pro/details/:pd_id/"
+            exact
+            component={Project_details}
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
