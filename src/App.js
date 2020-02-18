feafe90b4cc53cb8032ead295c59106cd4376dbe
hossainrkb <39438732+hossainrkb@users.noreply.github.com>
2020-02-18 13:35:50 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavTop from "./components/navtop";
 import Education from "./components/eductaions";
@@ -8,32 +8,94 @@ import Programming_languague from "./components/programming_languague";
 import Project from "./components/projects";
 import Project_details from "./components/pro_details";
 import Experience from "./components/experience";
+import { Redirect } from "react-router-dom";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="container">
-        <NavTop />
-        <Switch>
-          <Route path="/rakib_portfolio/" exact component={Introduction} />
-          <Route path="/rakib_portfolio/edu/" exact component={Education} />
-          <Route path="/rakib_portfolio/frmwrk/" exact component={Framework} />
-          <Route
-            path="/rakib_portfolio/pro_lan/"
-            exact
-            component={Programming_languague}
-          />
-          <Route path="/rakib_portfolio/proj/" exact component={Project} />
-          <Route path="/rakib_portfolio/exp/" exact component={Experience} />
-          <Route
-            path="/rakib_portfolio/pro/details/:pd_id/"
-            exact
-            component={Project_details}
-          />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+
+
+
+
+class App extends Component {
+  state = {
+    check:0
+  }
+  componentWillMount(){
+     if (window.location.href !== "http://localhost:3000/rakib_portfolio/") {
+       this.setState({
+         check:1
+       })
+      
+  }
+  }
+  render() {
+     if (this.state.check ==1) {
+       return (
+         <BrowserRouter>
+           <Redirect to="/rakib_portfolio/" />
+           <div className="container">
+             <NavTop />
+             <Switch>
+               <Route path="/rakib_portfolio/" exact component={Introduction} />
+               <Route
+                 path="/rakib_portfolio/edu/"
+                 exact
+                 component={Education}
+               />
+               <Route
+                 path="/rakib_portfolio/frmwrk/"
+                 exact
+                 component={Framework}
+               />
+               <Route
+                 path="/rakib_portfolio/pro_lan/"
+                 exact
+                 component={Programming_languague}
+               />
+               <Route path="/rakib_portfolio/proj/" exact component={Project} />
+               <Route
+                 path="/rakib_portfolio/exp/"
+                 exact
+                 component={Experience}
+               />
+               <Route
+                 path="/rakib_portfolio/pro/details/:pd_id/"
+                 exact
+                 component={Project_details}
+               />
+             </Switch>
+           </div>
+         </BrowserRouter>
+       );
+     }
+     return (
+       <BrowserRouter>
+         <div className="container">
+           <NavTop />
+           <Switch>
+             <Route path="/rakib_portfolio/" exact component={Introduction} />
+             <Route path="/rakib_portfolio/edu/" exact component={Education} />
+             <Route
+               path="/rakib_portfolio/frmwrk/"
+               exact
+               component={Framework}
+             />
+             <Route
+               path="/rakib_portfolio/pro_lan/"
+               exact
+               component={Programming_languague}
+             />
+             <Route path="/rakib_portfolio/proj/" exact component={Project} />
+             <Route path="/rakib_portfolio/exp/" exact component={Experience} />
+             <Route
+               path="/rakib_portfolio/pro/details/:pd_id/"
+               exact
+               component={Project_details}
+             />
+           </Switch>
+         </div>
+       </BrowserRouter>
+     );
+   
+  }
 }
 
-export default App;
+export default App
