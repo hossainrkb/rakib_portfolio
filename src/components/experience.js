0e5ca3afd97f451e092f4b4de822dcedd505b045
hossainrkb <39438732+hossainrkb@users.noreply.github.com>
2020-03-01 21:32:19 +0600

    Update experience.js

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
 class experience extends Component {
+  constructor(){
+    super()
+   
+  }
   render() {
+    
+     
     return (
       <div className="row">
         <div className="col-md-12">
@@ -27,15 +33,12 @@ class experience extends Component {
                     <td>Affable Technology</td>
                     <td>Web Developer (PHP)</td>
                     <td>June 2018–May 2019</td>
-                    
                   </tr>
-                    <tr>
+                  <tr>
                     <td>Tripovy.com</td>
                     <td>Web Developer (Laravel)</td>
                     <td>November 2019–January 2020</td>
-                    
                   </tr>
-               
                 </tbody>
               </table>
             </div>
