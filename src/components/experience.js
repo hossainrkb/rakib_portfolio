0ac66f3be217f6aabe1ecf6f152ccd518e3f6ed5
hossainrkb <39438732+hossainrkb@users.noreply.github.com>
2020-03-01 20:10:50 +0600

    Update experience.js

--- src/components/experience.js
+++ src/components/experience.js
@@ -26,7 +26,13 @@ class experience extends Component {
                   <tr>
                     <td>Affable Technology</td>
                     <td>Web Developer (PHP)</td>
-                    <td>June 2018–November 2019</td>
+                    <td>June 2018–May 2019</td>
+                    
+                  </tr>
+                    <tr>
+                    <td>Tripovy.com</td>
+                    <td>Web Developer (Laravel)</td>
+                    <td>November 2019–January 2020</td>
                     
                   </tr>
                
